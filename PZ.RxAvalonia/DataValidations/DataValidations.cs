981916d13bd598c853aa785406e2bf2b454de9a8
zjxszqc <zjxszqc@live.com>
2026-01-16 23:55:41 +0800

    improve data validations

--- PZ.RxAvalonia/DataValidations/DataValidations.cs
+++ PZ.RxAvalonia/DataValidations/DataValidations.cs
@@ -3,56 +3,80 @@
 public interface IDataValidation<T>
 {
     bool IsValid(T value);
-    string ErrorMessage { get; set; }
+    string ErrorMessage { get; }
 }
 
-public class RequiredValidation : IDataValidation<string?>
+public abstract class ValidationBase
 {
+    protected Func<ValidationBase, string>? _msgGetter;
+    public void CustomMessage<T>(Func<T, string> messageGetter) where T : ValidationBase
+    {
+        _msgGetter = (c) =>
+        {
+            if (c is T t)
+            {
+                return messageGetter(t);
+            }
+            throw new InvalidOperationException();
+        };
+    }
+}
+
+public class RequiredValidation : ValidationBase, IDataValidation<string?>
+{
+    public static Func<RequiredValidation, string> GlobalMsgGetter { get; set; } = (_) => "This field is required.";
+
     public bool IsValid(string? value)
     {
         return !string.IsNullOrWhiteSpace(value);
     }
-    public string ErrorMessage { get; set; } = "This field is required.";
+    public string ErrorMessage => _msgGetter is not null ? _msgGetter(this) : GlobalMsgGetter(this);
 }
-public class MaxLengthValidation(uint maxLength) : IDataValidation<string?>
+public class MaxLengthValidation(uint maxLength) : ValidationBase, IDataValidation<string?>
 {
-    public uint MaxLength { get; } = maxLength;
+    public static Func<MaxLengthValidation, string> GlobalMsgGetter { get; set; } = (v) => $"This field must be at most {v.MaxLength} characters long.";
+
+    public uint MaxLength { get; init; } = maxLength;
     public bool IsValid(string? value)
     {
         return (value?.Length ?? 0) <= MaxLength;
     }
-    public string ErrorMessage { get; set; } = $"This field must be at most {maxLength} characters long.";
+    public string ErrorMessage => _msgGetter is not null ? _msgGetter(this) : GlobalMsgGetter(this);
 }
-public class MinLengthValidation(uint minLength) : IDataValidation<string?>
+public class MinLengthValidation(uint minLength) : ValidationBase, IDataValidation<string?>
 {
+    public static Func<MinLengthValidation, string> GlobalMsgGetter { get; set; } = (v) => $"This field must be at least {v.MinLength} characters long.";
     public uint MinLength { get; } = minLength;
     public bool IsValid(string? value)
     {
         return (value?.Length ?? 0) >= MinLength;
     }
-    public string ErrorMessage { get; set; } = $"This field must be at least {minLength} characters long.";
+    public string ErrorMessage => _msgGetter is not null ? _msgGetter(this) : GlobalMsgGetter(this);
 }
-public class MaxValueValidation<T>(T maxValue) : IDataValidation<T?> where T : struct, IComparable<T>
+public class MaxValueValidation<T>(T maxValue) : ValidationBase, IDataValidation<T?> where T : struct, IComparable<T>
 {
+    public static Func<MaxValueValidation<T>, string> GlobalMsgGetter { get; set; } = (v) => $"This field must be at most {v.MaxValue}.";
+
     public T MaxValue { get; } = maxValue;
     public bool IsValid(T? value)
     {
         if (!value.HasValue) return false;
         return value.Value.CompareTo(MaxValue) <= 0;
     }
-    public string ErrorMessage { get; set; } = $"This field must be at most {maxValue}.";
+    public string ErrorMessage => _msgGetter is not null ? _msgGetter(this) : GlobalMsgGetter(this);
 }
-public class MinValueValidation<T>(T minValue) : IDataValidation<T?> where T : struct, IComparable<T>
+public class MinValueValidation<T>(T minValue) : ValidationBase, IDataValidation<T?> where T : struct, IComparable<T>
 {
+    public static Func<MinValueValidation<T>, string> GlobalMsgGetter { get; set; } = (v) => $"This field must be at least {v.MinValue}.";
+
     public T MinValue { get; } = minValue;
     public bool IsValid(T? value)
     {
         if (!value.HasValue) return false;
         return value.Value.CompareTo(MinValue) >= 0;
     }
-    public string ErrorMessage { get; set; } = $"This field must be at least {minValue}.";
+    public string ErrorMessage => _msgGetter is not null ? _msgGetter(this) : GlobalMsgGetter(this);
 }
-
 
 public static class DataValidations
 {
---
+++ PZ.RxAvalonia/FuncTemplete.cs
@@ -0,0 +1,48 @@
+﻿using Avalonia.Controls;
+using Avalonia.Controls.Templates;
+using Avalonia.Styling;
+
+namespace PZ.RxAvalonia;
+
+public class FuncTemplete<T>(Func<T> Builder, ComponentBase Parent) : ITemplate<T> where T : Control
+{
+    public T Build()
+    {
+        using var context = new ComponentBuildContext(Parent);
+        context.SetState(ComponentBuildState.ViewBuilding);
+        var content = Builder();
+        context.SetState(ComponentBuildState.None);
+
+        return content;
+    }
+
+    object? ITemplate.Build()
+    {
+        return Build();
+    }
+}
+
+public class FuncDataTemplete<TData, TControl>(Func<TData, TControl> Builder, ComponentBase Parent) : IDataTemplate where TControl: Control
+{
+    public TControl Build(TData d)
+    {
+        using var context = new ComponentBuildContext(Parent);
+        context.SetState(ComponentBuildState.ViewBuilding);
+        var content = Builder(d);
+        context.SetState(ComponentBuildState.None);
+
+        return content;
+    }
+
+    public bool Match(object? data)
+    {
+        return data is TData;
+    }
+
+    Control? ITemplate<object?, Control?>.Build(object? param)
+    {
+        if (param is not TData d) throw new InvalidOperationException("Templete param type invalid!");
+
+        return Build(d);
+    }
+}
